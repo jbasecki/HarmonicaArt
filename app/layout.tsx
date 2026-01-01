206203cc380be366df122070ddeae5928c5b037e
Joanne Basecki <jbasecki@users.noreply.github.com>
2026-01-01 03:03:27 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Harmonica",
+  description: "A Sanctuary for Stashed Cognition",
+  openGraph: {
+    title: "Harmonica",
+    images: [{ url: "/opengraph-image.jpg", width: 1200, height: 630 }],
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
